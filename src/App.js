974a4bc3d41b67f072b069ed1b0344022a1f84ed
Lasse Minet <78145641+LasseML@users.noreply.github.com>
2021-03-09 15:55:02 +0100

    feat: added headerComponent to pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import LoginPage from "./compenents/pages/LoginPage"
 import Profile from "./compenents/pages/Profile"
 import PrivateRoute from "./compenents/PrivateRoute"
+import AppHeader from "./compenents/AppHeader"
 import TranslateComponent from "./compenents/pages/translater/TranslateComponent";
 
 function App() {
   return (
     <div>
         <Router>
-            {/*<AppHeader />*/}
+            {<AppHeader />}
             <Switch>
                 <Route path='/' exact component={LoginPage} />
                 <PrivateRoute path='/translate' component={TranslateComponent} />
---
+++ src/compenents/AppHeader.js
@@ -0,0 +1,27 @@
+import {useHistory} from "react-router-dom";
+import Button from "./Button";
+
+const divStyle = {
+    float: 'right',
+};
+
+const AppHeader = () => {
+    const history = useHistory()
+    const buttonText = sessionStorage.getItem('userName') + "'s Profile"
+    const changePage = () => {
+        history.push('/profile')
+    }
+
+    return (
+        <div className='nav-bar'>
+            <ul>
+                <li><h3>Translate App!</h3></li>
+                <li style={divStyle}>
+                    <Button text={buttonText} onClick={changePage} />
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default AppHeader
