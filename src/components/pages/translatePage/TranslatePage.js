609eacae146ec34b0beda38a63feb3e96db22565
Rasmus Ulrichsen <14159069+Maskinexpert@users.noreply.github.com>
2021-03-09 17:06:55 +0100

    doc: comments for onTranslate added

--- src/components/pages/translatePage/TranslatePage.js
+++ src/components/pages/translatePage/TranslatePage.js
@@ -4,7 +4,12 @@ import TranslateInput from './TranslateInput'
 
 function TranslatePage() {
     const [translate, setTranslate] = useState('')
-
+/*
+* onTranslate takes a text to translate as input
+* it retrieves the current translation history from session storage
+* if the text already exists it places the text in the front of the translation history
+* else it places the text in front of the translation history and removes the oldest translation if more than 10 are present
+*/
     const onTranslate =(textToTranslate) => {
         const translateHistory = JSON.parse(sessionStorage.getItem('transArr'))
         const index = translateHistory.indexOf(textToTranslate.toUpperCase())
